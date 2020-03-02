ffaba1beff65dc20f4f157328846cd1e3f4b7b27
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2020-03-02 19:16:32 +0100

    Added full implementation of std::unique_ptr. Next up is shared_ptr

--- src/unique_ptr.hpp
+++ src/unique_ptr.hpp
@@ -18,39 +18,59 @@
 #ifndef UNIQUE_PTR_HPP
 #define UNIQUE_PTR_HPP
 #include <utillities.h>
+#include "default_deleters.hpp"
 
 namespace std {
 
-	template<typename T>
+	template<typename T, typename D = default_deleter<T>>
 	class unique_ptr {
+        using Tt = std::remove_array_t<T>;
+        template<typename L>
+        using is_derived = std::conditional_t<
+            std::is_class<std::remove_array_t<L>>::value,
+            std::is_base_of<Tt,std::remove_array_t<L>>,
+            std::is_same<Tt,std::remove_array_t<L>>
+        >;
 	public:
-		explicit unique_ptr(T* res) : resource{res} 
-		{ }
-		~unique_ptr() { release_resource(); }
+        unique_ptr(const unique_ptr&) = delete;
+        unique_ptr() = delete;
+        template<typename L>
+        unique_ptr(unique_ptr<L>&& ptr)
+         : resource(static_cast<Tt*>(ptr.get()))
+        { static_assert(is_derived<L>::value, "Only unique pointers of derived classes can be moved"); }
+        unique_ptr(Tt* ptr) : resource{ptr} {}
+        ~unique_ptr() { release_resource(); }
+        // No implicit copy assignment, please
+        unique_ptr<T,D>& operator=(const unique_ptr<T,D>& ptr) = delete;
 		
-		inline T* const operator->() const { return resource; }
-		inline T& operator*() const { return *resource; }
-		
-		void move(unique_ptr<T>& other) { // Move 'this' into 'other'
-			other.release_resource();
-			other.resource = resource;
-			resource = NULL;
-		}
-		
-		inline T* const get() const { return resource; }
+        inline Tt& operator[](const unsigned char i) const {
+            static_assert(std::is_array<T>::value,
+            "'[]' operator only work on pointers to arrays");
+        }
+        inline T* operator->() const {
+            static_assert(!std::is_array<T>::value,
+            "'->' operator doesn't work on pointers to arrays");
+            return resource;
+        }
+        inline T& operator*() const {
+            static_assert(!std::is_array<T>::value,
+            "'*' operator doesn't work on pointers to arrays");
+            return *resource;
+        }
+        inline Tt* const get() const { return resource; }
+        template<typename L>
+        unique_ptr& operator=(unique_ptr<L>&& p) {
+            static_assert(is_derived<L>::value, "Can only move pointers of derived classes!");
+            if (resource != NULL)
+                release_resource();
+            resource = p.resource; // move ownership of the resource to this
+            p.resource = NULL;
+            return *this;
+        }
 		
 	private:
-		T* resource;
-		// Disable default construction and copying
-		unique_ptr() = delete;
-		unique_ptr(const unique_ptr<T>&) = delete;
-
-		void release_resource() {
-			delete resource;
-			// --- ↓ --- //
-			resource->~T();
-			free(resource);
-		}
+		Tt* resource;
+		void release_resource() { D::free(resource); }
 	};
 
 }
